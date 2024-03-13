ea4a7ec26dc76666c3c7208a2b5cbf9114c584c2
Madhav Murari <114315179+Madhav-Murari@users.noreply.github.com>
2024-03-13 19:43:42 +0530

    change in data in year

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -103,7 +103,7 @@ function Footer() {
           <SocialMediaIcon href={Bio.insta} target="display"><InstagramIcon /></SocialMediaIcon>
         </SocialMediaIcons>
         <Copyright>
-          &copy; 2023 Madhav Murari. All rights reserved.
+          &copy; 2024 Madhav Murari. All rights reserved.
         </Copyright>
 
       </FooterWrapper>
